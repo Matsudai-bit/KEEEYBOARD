2b4a59f51e8b680b30fa65b8dce7449322542aac
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2026-02-06 12:27:18 +0900

    タイトルシーン実装

--- Assets/Develop/Scripts/Title/SelectorManager.cs
+++ Assets/Develop/Scripts/Title/SelectorManager.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using static UnityEngine.GraphicsBuffer;
 
 public class SelectorManager : MonoBehaviour
@@ -10,6 +11,13 @@ public class SelectorManager : MonoBehaviour
     [SerializeField]
     private GameObject endSelecter;
 
+    [Header("シーンのフェードインエフェクト")]
+    [SerializeField]
+    SceneTransitionEffect m_sceneFadeInEffect;
+    [Header("シーンのフェードアウトエフェクト")]
+    [SerializeField]
+    SceneTransitionEffect m_sceneFadeOutEffect;
+
     // 現在の選択状態
     private bool m_isStartSelecting = true;
 
@@ -19,6 +27,8 @@ public class SelectorManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        m_sceneFadeInEffect.StartTransition(() => { });
+
         // 規則的な点滅パターンを作成
         flickerSequence = DOTween.Sequence();
         flickerSequence.Append(startSelecter.GetComponent<UnityEngine.UI.Image>().DOFade(0.2f, 0.02f));
@@ -28,20 +38,63 @@ public class SelectorManager : MonoBehaviour
         flickerSequence.Append(endSelecter.GetComponent<UnityEngine.UI.Image>().DOFade(1.0f, 0.02f));
         flickerSequence.AppendInterval(0.5f);
         flickerSequence.SetLoops(-1); // 無限ループ
-
+                                      // セレクターの表示切り替え
+        if (m_isStartSelecting)
+        {
+            startSelecter.SetActive(true);
+            endSelecter.SetActive(false);
+        }
+        else
+        {
+            startSelecter.SetActive(false);
+            endSelecter.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 入力受け付け
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            m_isStartSelecting = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+
+    }
+
+    public void OnSubmit(InputAction.CallbackContext context)
+    {
+        
+        m_sceneFadeOutEffect.StartTransition(() => {
+            if (m_isStartSelecting)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("StageSelect");
+            }
+            else
+            {
+
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+                Application.Quit();//ゲームプレイ終了
+#endif
+            }
+        });
+
+   
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (!context.performed) { return; }
+        var axis = context.ReadValue<Vector2>();
+        if (Mathf.Approximately(axis.magnitude, 0.0f)) { return; }
+        bool right = (axis.x > 0.0f) ? true : false;
+
+        if (right)
         {
             m_isStartSelecting = false;
+
+        }
+        else
+        {
+            m_isStartSelecting = true;
+
         }
 
         // セレクターの表示切り替え
@@ -55,6 +108,5 @@ public class SelectorManager : MonoBehaviour
             startSelecter.SetActive(false);
             endSelecter.SetActive(true);
         }
-
     }
 }
